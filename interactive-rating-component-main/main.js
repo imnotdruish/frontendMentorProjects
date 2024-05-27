76a96c7a3778fade7260636c498fe121cbc63f41
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2024-05-27 19:43:29 -0400

    added variables for javascript

---
+++ interactive-rating-component-main/main.js
@@ -0,0 +1,5 @@
+let ratingInput = document.getElementsByClassName('rating__number');
+let ratingSelection = document.getElementsByTagName('span');
+let hideRating = document.getElementsByClassName('rating__container');
+let showSelection = document.getElementsByClassName('selection__container');
+
