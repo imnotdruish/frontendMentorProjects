585a06a28cc011ca55af6bda8b275deb6f93307b
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2024-05-29 20:17:42 -0400

    added submit button variable

--- interactive-rating-component-main/main.js
+++ interactive-rating-component-main/main.js
@@ -2,4 +2,4 @@ let ratingInput = document.getElementsByClassName('rating__number');
 let ratingSelection = document.getElementsByTagName('span');
 let hideRating = document.getElementsByClassName('rating__container');
 let showSelection = document.getElementsByClassName('selection__container');
-
+let submitBtn = document.getElementById('submit');
