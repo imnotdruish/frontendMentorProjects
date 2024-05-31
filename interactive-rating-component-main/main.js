f4dcc64e5779c2be39c5c1ee0f809a0b54206c1e
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2024-05-31 17:53:31 -0400

    starting onClick events for rating buttons

--- interactive-rating-component-main/main.js
+++ interactive-rating-component-main/main.js
@@ -3,3 +3,10 @@ let ratingSelection = document.getElementsByTagName('span');
 let hideRating = document.getElementsByClassName('rating__container');
 let showSelection = document.getElementsByClassName('selection__container');
 let submitBtn = document.getElementById('submit');
+
+let ratingBtn1 = (event) => {
+
+  let rating = document.getElementById('1').value;
+  let userRating = document.getElementById('user__rating');
+  userRating.innerText = rating;
+}
