7c2d7f392003357bcf575c596d3cb6db19c12f68
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2024-06-01 23:53:48 -0400

    added code for other 4 buttons

--- interactive-rating-component-main/main.js
+++ interactive-rating-component-main/main.js
@@ -10,3 +10,31 @@ let ratingBtn1 = (event) => {
   let userRating = document.getElementById('user__rating');
   userRating.innerText = rating;
 }
+
+let ratingBtn2= (event) => {
+
+  let rating = document.getElementById('2').value;
+  let userRating = document.getElementById('user__rating');
+  userRating.innerText = rating;
+}
+
+let ratingBtn3 = (event) => {
+
+  let rating = document.getElementById('3').value;
+  let userRating = document.getElementById('user__rating');
+  userRating.innerText = rating;
+}
+
+let ratingBtn4 = (event) => {
+
+  let rating = document.getElementById('4').value;
+  let userRating = document.getElementById('user__rating');
+  userRating.innerText = rating;
+}
+
+let ratingBtn5 = (event) => {
+
+  let rating = document.getElementById('5').value;
+  let userRating = document.getElementById('user__rating');
+  userRating.innerText = rating;
+}
