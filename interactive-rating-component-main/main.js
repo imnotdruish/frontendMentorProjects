0583e4f71e4cf296839b8914d06175d732cd433b
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2024-06-02 00:19:02 -0400

    testing options for click or select events

--- interactive-rating-component-main/main.js
+++ interactive-rating-component-main/main.js
@@ -1,40 +1,45 @@
-let ratingInput = document.getElementsByClassName('rating__number');
-let ratingSelection = document.getElementsByTagName('span');
-let hideRating = document.getElementsByClassName('rating__container');
-let showSelection = document.getElementsByClassName('selection__container');
+let ratingInput = document.getElementByClassName('rating__number');
+let ratingSelection = document.getElementByTagName('span');
+let hideRating = document.getElementByClassName('rating__container');
+let showSelection = document.getElementByClassName('selection__container');
+let ratingBtn1 = document.getElementById('1');
+let ratingBtn2 = document.getElementById('2');
+let ratingBtn3 = document.getElementById('3');
+let ratingBtn4 = document.getElementById('4');
+let ratingBtn5 = document.getElementById('5');
+let rating = "";
+let userRating = document.getElementById("userRating");
 let submitBtn = document.getElementById('submit');
 
-let ratingBtn1 = (event) => {
-
-  let rating = document.getElementById('1').value;
-  let userRating = document.getElementById('user__rating');
-  userRating.innerText = rating;
-}
-
-let ratingBtn2= (event) => {
+ratingBtn1.addEventListener('click', function(){
+  userRating = 1;
+});
+/*
+ratingBtn2.addEventListener("click", (event) => {
 
   let rating = document.getElementById('2').value;
   let userRating = document.getElementById('user__rating');
   userRating.innerText = rating;
-}
+})
 
-let ratingBtn3 = (event) => {
+ratingBtn3.addEventListener("click", (event) => {
 
   let rating = document.getElementById('3').value;
   let userRating = document.getElementById('user__rating');
   userRating.innerText = rating;
-}
+})
 
-let ratingBtn4 = (event) => {
+ratingBtn4.addEventListener("click", (event) => {
 
   let rating = document.getElementById('4').value;
   let userRating = document.getElementById('user__rating');
   userRating.innerText = rating;
-}
+})
 
-let ratingBtn5 = (event) => {
+ratingBtn5.addEventListener("click", (event) => {
 
   let rating = document.getElementById('5').value;
   let userRating = document.getElementById('user__rating');
   userRating.innerText = rating;
-}
+})
+*/
