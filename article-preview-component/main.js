b0970fe7d73f9b8279c10c33cfece0b4cf7fa281
imnotdruish <105814537+imnotdruish@users.noreply.github.com>
2024-06-17 13:46:22 -0400

    Working with Pseudo Elements

--- article-preview-component/main.js
+++ article-preview-component/main.js
@@ -1,26 +1,65 @@
 let share = document.querySelector('.fa-share');
 let profile = document.querySelector('.profile');
 let social = document.querySelector('.social__links');
+let popupSocial = document.querySelector('.popup__social__links');
 let card = document.querySelector('.profile__card');
+const width = window.innerWidth;
+
+console.log(width);
 
 share.addEventListener('click', () => {
-  if (social.style.display == 'none') {    
-      social.style.display = 'flex';
-      card.style.display = 'none';
-      profile.style.marginTop = '30px';
-      profile.classList.add('dark');
-      share.classList.add('dark');
-      share.classList.remove('light');
-      share.style.backgroundColor = "var(--clr-neutral)";
-      share.style.color = "var(--clr-neutral-light)";
-  } else {
-      social.style.display = 'none';
-      card.style.display = 'flex';
-      profile.style.marginTop = 'unset';
-      profile.classList.remove('dark');
-      share.classList.remove('dark');
-      share.classList.add('light');
-      share.style.backgroundColor = "var(--clr-neutral-light)";
-      share.style.color = "var(--clr-neutral)";
+  if (width < 999) {
+
+    if (social.style.display == 'none') {   
+
+        social.style.display = 'flex';
+        card.style.display = 'none';
+        profile.style.marginTop = '30px';
+        profile.classList.add('dark');
+        share.classList.add('dark');
+        share.classList.remove('light');
+        share.style.backgroundColor = "var(--clr-neutral)";
+        share.style.color = "var(--clr-neutral-light)";
+
+    } else {
+
+        social.style.display = 'none';
+        card.style.display = 'flex';
+        profile.style.marginTop = 'unset';
+        profile.classList.remove('dark');
+        share.classList.remove('dark');
+        share.classList.add('light');
+        share.style.backgroundColor = "var(--clr-neutral-light)";
+        share.style.color = "var(--clr-neutral)";
+
     }
+
+  } else {
+    
+    if (social.style.display == 'none') {
+      
+        popupSocial.style.display = 'flex';
+        card.style.display = 'none';
+        profile.style.marginTop = '30px';
+        profile.classList.add('dark');
+        share.classList.add('dark');
+        share.classList.remove('light');
+        share.style.backgroundColor = "var(--clr-neutral)";
+        share.style.color = "var(--clr-neutral-light)";
+
+    } else {
+
+        popupSocial.style.display = 'none';
+        card.style.display = 'flex';
+        profile.style.marginTop = 'unset';
+        profile.classList.remove('dark');
+        share.classList.remove('dark');
+        share.classList.add('light');
+        share.style.backgroundColor = "var(--clr-neutral-light)";
+        share.style.color = "var(--clr-neutral)";
+
+    }
+
+  }
+
 });
